9beb365d4a52fe805440b5a64f1973142039944d
Ja <ja.techup@gmail.com>
2024-07-17 14:22:32 +0700

    feat: ex1-2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,36 @@
+students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+searchStudent = "John";
+
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let left = 0;
+  let right = students.length - 1;
+  let index = -1;
+
+  while (left < right) {
+    let mid = Math.floor((right + left) / 2);
+    if (searchStudent === students[mid]) {
+      index = mid;
+      break;
+    } else if (searchStudent > students[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return index;
 }
 
+console.log(findStudentIndex(students, searchStudent));
+
 // ตอบคำถามตรงนี้จ้า
+// Big O = log n เพราะทุกการ search สามารถลดจำนวนที่ต้องค้นหาได้ครึ่งนึง
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,35 @@
+const books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+
+const searchBook = "The Master Mind";
+
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+
+  let left = 0;
+  let right = books.length - 1;
+  let index = -1;
+
+  while (left < right) {
+    let mid = Math.floor((right + left) / 2);
+    if (searchTitle === books[mid].title) {
+      index = mid;
+      break;
+    } else if (searchTitle > books[mid].title) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return index;
 }
+
+console.log(findBookIndex(books, searchBook));
